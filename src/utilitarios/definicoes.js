81cfa1c513fb3f4382d190a002456efb6ee14917
Alex <alexsnider8@gmail.com>
2023-10-06 14:12:21 -0300

    5.0.1 - url campo added

--- src/utilitarios/definicoes.js
+++ src/utilitarios/definicoes.js
@@ -1,8 +1,9 @@
-/* export const urlBase = "https://129.146.68.51/aluno2-pfsii/agendamento";
-export const urlBase2 = "https://129.146.68.51/aluno2-pfsii/usuario"; */
+export const urlBase = "https://129.146.68.51/aluno2-pfsii/agendamento";
+export const urlBase2 = "https://129.146.68.51/aluno2-pfsii/usuario";
+export const urlBase3 = "https://129.146.68.51/aluno2-pfsii/campo";
 
-export const urlBase = "http://localhost:3008/agendamento";
+/* export const urlBase = "http://localhost:3008/agendamento";
 export const urlBase2 = "http://localhost:3007/usuario";
-export const urlBase3 = "http://localhost:3009/campo";
+export const urlBase3 = "http://localhost:3009/campo"; */
 
 
